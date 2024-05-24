2ff415a7f20c149c816142180af3874f45fd2033
Douglas Kim <dougkim@15solutions.com>
2024-05-24 17:14:04 +0200

    Install JSON Forms Vue, Vuetify-based renderer set for JSON Forms, and Material Design icons.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+          '@mdi': path.resolve(__dirname, 'node_modules/@mdi/font/css/materialdesignicons.min.css')
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
