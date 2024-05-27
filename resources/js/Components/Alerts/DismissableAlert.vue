dce22f497d7a40fae0a2648f8bc9739eacdec918
Douglas Kim <dougkim@15solutions.com>
2024-05-27 17:59:52 +0200

    Implement DismissableAlert component used to display success and error messages when creating and updating resources in the CMS.

---
+++ resources/js/Components/Alerts/DismissableAlert.vue
@@ -0,0 +1,34 @@
+<template>
+  <VFadeTransition>
+    <VAlert
+      v-if="alert !== null"
+      :type="alert.type"
+      dismissible>
+      {{ alert.message }}
+    </VAlert>
+  </VFadeTransition>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import useEmitter from '@/composables/useEmitter'
+
+  const emitter = useEmitter()
+
+  const alert = ref(null)
+
+  emitter.on('show-dismissable-alert', (payload) => {
+    alert.value = payload
+    if (payload && payload.hasOwnProperty('timeout')) {
+      setTimeout(() => {
+        alert.value = null
+      }, payload.timeout)
+    }
+  })
+</script>
+
+<style lang="postcss" scoped>
+  .v-alert {
+    @apply fixed top-12 left-1/2 transform -translate-x-1/2 w-fit max-w-md
+  }
+</style>
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,6 +7,7 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import DismissableAlert from '@/Components/Alerts/DismissableAlert.vue'
 
 defineProps({
     title: String,
@@ -303,6 +304,8 @@ const logout = () => {
             <!-- Page Content -->
             <main>
                 <slot />
+
+                <DismissableAlert />
             </main>
         </div>
     </div>
