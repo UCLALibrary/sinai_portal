50e2851d27e9edec1573519f53e285506195fbf5
Douglas Kim <dougkim@15solutions.com>
2024-05-27 17:55:56 +0200

    Integrate mitt library and implement composable to emit events across components.

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,10 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+// mitt
+import mitt from 'mitt'
+const emitter = mitt()
+
 // vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
@@ -23,11 +27,13 @@ createInertiaApp({
     title: (title) => `${title} | ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
+        app.config.globalProperties.emitter = emitter
+        return app
             .use(plugin)
             .use(ZiggyVue)
             .use(vuetify)
-            .mount(el);
+            .mount(el)
     },
     progress: {
         color: '#4B5563',
