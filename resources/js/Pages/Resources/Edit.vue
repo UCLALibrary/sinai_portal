3e67acc833d3c9b6d8d8c4ffc0ad5d4a3a623ab7
Douglas Kim <dougkim@15solutions.com>
2024-06-28 16:38:47 -0700

    Fix issue with saving JSON form data when editing records. Refactor Edit component to use Vue Composition API instead of Options API.

--- resources/js/Pages/Resources/Edit.vue
+++ resources/js/Pages/Resources/Edit.vue
@@ -23,83 +23,54 @@
   </AppLayout>
 </template>
 
-<script>
-  import { defineComponent, defineAsyncComponent, ref, onMounted } from 'vue'
+<script setup>
+  import { defineAsyncComponent } from 'vue'
+  import useEmitter from '@/composables/useEmitter'
   import AppLayout from '@/Layouts/AppLayout.vue'
   const CreateEditForm = defineAsyncComponent(() => import('@/jsonforms/components/CreateEditForm.vue'))
-  import useEmitter from '@/composables/useEmitter'
 
-  export default defineComponent({
-    name: 'Edit',
-
-    components: {
-      AppLayout,
-      CreateEditForm,
-    },
-
-    props: {
-      schema: { type: Object, required: true },
-      uischema: { type: Object, required: true },
-      data: { type: Object, required: false, default: null },
-      saveEndpoint: { type: String, required: true },
-      redirectUrl: { type: String, required: true },
-    },
-
-    setup(props) {
-      const emitter = useEmitter()
-
-      const data = ref({})
-
-      onMounted(() => {
-        // initialize the form with the supplied metadata
-        data.value = props.data ?? {}
-      })
-
-      const onChange = (event) => {
-        data.value = event.data
-      }
-
-      const onSave = () => {
-        axios.patch(props.saveEndpoint, {
-          json: data.value,
-        }).then(() => {
-          window.location.href = props.redirectUrl
-        }).catch(error => {
-          // display alert that there was an error saving the resource
-          emitter.emit('show-dismissable-alert', {
-            type: 'error',
-            message: 'Error saving. Please check your form for errors.',
-            timeout: 2000,
-          })
-        })
-      }
-
-      const onSaveAndContinue = () => {
-        axios.patch(props.saveEndpoint, {
-          json: data.value,
-        }).then(() => {
-          // display alert that the resource has been saved
-          emitter.emit('show-dismissable-alert', {
-            type: 'success',
-            message: 'Saved successfully. Please continue editing.',
-            timeout: 2000,
-          })
-        }).catch(error => {
-          // display alert that there was an error saving the resource
-          emitter.emit('show-dismissable-alert', {
-            type: 'error',
-            message: 'Error saving. Please check your form for errors.',
-            timeout: 2000,
-          })
-        })
-      }
-
-      return {
-        data,
-        onChange,
-        onSave,
-        onSaveAndContinue,
-      }
-    }
+  const props = defineProps({
+    schema: { type: Object, required: true },
+    uischema: { type: Object, required: true },
+    data: { type: Object, required: false, default: null },
+    saveEndpoint: { type: String, required: true },
+    redirectUrl: { type: String, required: true },
   })
+
+  const emitter = useEmitter()
+
+  const onSave = (jsonData) => {
+    axios.put(props.saveEndpoint, {
+      json: jsonData,
+    }).then(() => {
+      window.location.href = props.redirectUrl
+    }).catch(error => {
+      // display alert that there was an error saving the resource
+      emitter.emit('show-dismissable-alert', {
+        type: 'error',
+        message: 'Error saving. Please check your form for errors.',
+        timeout: 2000,
+      })
+    })
+  }
+
+  const onSaveAndContinue = (jsonData) => {
+    axios.put(props.saveEndpoint, {
+      json: jsonData.value,
+    }).then(() => {
+      // display alert that the resource has been saved
+      emitter.emit('show-dismissable-alert', {
+        type: 'success',
+        message: 'Saved successfully. Please continue editing.',
+        timeout: 2000,
+      })
+    }).catch(error => {
+      // display alert that there was an error saving the resource
+      emitter.emit('show-dismissable-alert', {
+        type: 'error',
+        message: 'Error saving. Please check your form for errors.',
+        timeout: 2000,
+      })
+    })
+  }
 </script>
