7c9c39adb9c80c928ba5ebd0f1d3502d81242b4c
Douglas Kim <dougkim@15solutions.com>
2024-06-28 16:37:52 -0700

    Refactor Create component to use Vue Composition API instead of Options API.

--- resources/js/Pages/Resources/Create.vue
+++ resources/js/Pages/Resources/Create.vue
@@ -23,56 +23,48 @@
   </AppLayout>
 </template>
 
-<script>
-  import { defineComponent, defineAsyncComponent, ref } from 'vue'
+<script setup>
+  import { defineAsyncComponent, ref } from 'vue'
+  import useEmitter from '@/composables/useEmitter'
   import AppLayout from '@/Layouts/AppLayout.vue'
   const CreateEditForm = defineAsyncComponent(() => import('@/jsonforms/components/CreateEditForm.vue'))
-  import useEmitter from '@/composables/useEmitter'
 
-  export default defineComponent({
-    name: 'Create',
-
-    components: {
-      AppLayout,
-      CreateEditForm,
-    },
-
-    props: {
-      schema: { type: Object, required: true },
-      uischema: { type: Object, required: true },
-      saveEndpoint: { type: String, required: true },
-      redirectUrl: { type: String, required: true },
-    },
-
-    setup(props) {
-      const emitter = useEmitter()
-
-      const data = ref({})
-
-      const onSave = (jsonData) => {
-        axios.post(props.saveEndpoint, {
-          json: jsonData,
-        }).then(() => {
-          window.location.href = props.redirectUrl
-        }).catch(error => {
-          // display alert that there was an error saving the resource
-          emitter.emit('show-dismissable-alert', {
-            type: 'error',
-            message: 'Error saving. Please check your form for errors.',
-            timeout: 2000,
-          })
-        })
-      }
-
-      const onCancel = () => {
-        window.location.href = props.redirectUrl
-      }
-
-      return {
-        data,
-        onSave,
-        onCancel,
-      }
-    }
+  const props = defineProps({
+    schema: { type: Object, required: true },
+    uischema: { type: Object, required: true },
+    saveEndpoint: { type: String, required: true },
+    redirectUrl: { type: String, required: true },
   })
+
+  const emitter = useEmitter()
+
+  const data = ref({
+    ark: 'ark:/21198/z1.111111',
+    type: 'shelf',
+    idno: [
+      {
+        type: 'shelfmark',
+        value: 'MS. 123',
+      },
+    ],
+  })
+
+  const onSave = (jsonData) => {
+    axios.post(props.saveEndpoint, {
+      json: jsonData,
+    }).then(() => {
+      window.location.href = props.redirectUrl
+    }).catch(error => {
+      // display alert that there was an error saving the resource
+      emitter.emit('show-dismissable-alert', {
+        type: 'error',
+        message: 'Error saving. Please check your form for errors.',
+        timeout: 2000,
+      })
+    })
+  }
+
+  const onCancel = () => {
+    window.location.href = props.redirectUrl
+  }
 </script>
