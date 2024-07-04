04b8dd5b61943b072ac85496b8f472e4af37e2b0
Douglas Kim <dougkim@15solutions.com>
2024-07-03 17:10:54 -0700

    Update markup and styling of cancel and save buttons on CreateEditForm component.

--- resources/js/jsonforms/components/CreateEditForm.vue
+++ resources/js/jsonforms/components/CreateEditForm.vue
@@ -12,37 +12,41 @@
     <v-container>
       <v-row>
         <v-col class="flex justify-end space-x-4">
-          <v-btn
-            color="red"
-            @click="onCancel">
+          <button
+            type="button"
+            @click="onCancel"
+            class="cancel-button">
             Cancel
-          </v-btn>
+          </button>
 
           <template v-if="mode == 'create'">
-            <v-btn
-              color="gray"
+            <button
+              type="button"
               @click="onSave(false)"
-              :style="!isValid ? { cursor: 'not-allowed', 'pointer-events': 'auto' } : {}"
+              class="save-button"
+              :class="{ 'cursor-not-allowed pointer-events-auto opacity-50': !isValid }"
               :disabled="!isValid">
               Save
-            </v-btn>
+            </button>
           </template>
           <template v-else-if="mode == 'edit'">
-            <v-btn
-              color="gray"
+            <button
+              type="button"
               @click="onSave(true)"
-              :style="!isValid ? { cursor: 'not-allowed', 'pointer-events': 'auto' } : {}"
+              class="save-button"
+              :class="{ 'cursor-not-allowed pointer-events-auto opacity-50': !isValid }"
               :disabled="!isValid">
-              Save and Continue
-            </v-btn>
-          
-            <v-btn
-              color="gray"
+              Save &amp; Continue
+            </button>
+
+            <button
+              type="button"
               @click="onSave(false)"
-              :style="!isValid ? { cursor: 'not-allowed', 'pointer-events': 'auto' } : {}"
+              class="save-button"
+              :class="{ 'cursor-not-allowed pointer-events-auto opacity-50': !isValid }"
               :disabled="!isValid">
-              Save and Finish
-            </v-btn>
+              Save &amp; Finish
+            </button>
           </template>
         </v-col>
       </v-row>
@@ -107,3 +111,13 @@
     emit('on-cancel')
   }
 </script>
+
+<style lang="postcss" scoped>
+  .cancel-button {
+    @apply block rounded-md bg-sinai-red px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-sinai-red focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sinai-red
+  }
+
+  .save-button {
+    @apply block rounded-md bg-gray-500 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500
+  }
+</style>
