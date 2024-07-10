f787fd3069301f6a91c4b65491d4ce4b2061d6af
Douglas Kim <dougkim@15solutions.com>
2024-07-10 14:30:27 -0700

    Remove duplicate import of Material Design Icons stylesheet.

--- resources/js/vuetify.js
+++ resources/js/vuetify.js
@@ -2,7 +2,6 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
 
 const customTheme = {
   dark: false,
