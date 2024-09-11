0afe21c7fc9e04b572ad66fd4ab084ef0161fa04
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2024-09-11 17:33:18 +0200

    Add custom styles files to Tailwind config so the classes are detected

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        './resources/js/jsonforms/styles/*.js',
     ],
 
     theme: {
