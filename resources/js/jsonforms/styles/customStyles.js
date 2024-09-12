9477d8de4e4a5465a90e8e58b72216737f41a2d6
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2024-09-12 18:54:55 +0200

    Update custom styles for form elements

--- resources/js/jsonforms/styles/customStyles.js
+++ resources/js/jsonforms/styles/customStyles.js
@@ -2,14 +2,14 @@ const customStyles = {
   // extend defaults
   control: {
     root: 'flex flex-col gap-y-2',
-    wrapper: 'flex-1',
-    label: 'block text-sm font-medium text-gray-700 min-w-36',
-    input: 'w-full border border-gray-300 rounded-md shadow-sm focus:ring-sinai-red focus:border-sinai-red sm:text-sm bg-white max-w-3xl disabled:cursor-not-allowed',
+    wrapper: 'wrapper flex flex-1 items-center gap-x-2',
+    label: 'flex text-sm font-medium text-gray-700 min-w-44',
+    input: 'flex border border-gray-300 rounded-md shadow-sm focus:ring-sinai-red focus:border-sinai-red sm:text-sm bg-white w-full max-w-3xl disabled:cursor-not-allowed disabled:!text-gray-500',
     checkbox: 'border border-gray-300 rounded-md shadow-sm focus:ring-sinai-red focus:border-sinai-red sm:text-sm cursor-pointer',
-    textarea: 'border border-gray-300 rounded-md shadow-sm focus:ring-sinai-red focus:border-sinai-red sm:text-sm bg-white',
-    select: 'w-full border border-gray-300 rounded-md shadow-sm focus:ring-sinai-red focus:border-sinai-red sm:text-sm bg-white',
+    textarea: 'border border-gray-300 rounded-md shadow-sm focus:ring-sinai-red focus:border-sinai-red sm:text-sm bg-white w-full ml-2 max-w-3xl',
+    select: 'w-full border border-gray-300 rounded-md shadow-sm focus:ring-sinai-red focus:border-sinai-red sm:text-sm bg-white max-w-3xl',
     error: 'text-xs text-red-500 ml-1',
-    asterisk: 'text-red-500',
+    asterisk: 'ml-0.5 text-red-500',
   },
   verticalLayout: {
     root: 'flex flex-col gap-y-2',
@@ -25,7 +25,7 @@ const customStyles = {
     root: 'border border-gray-300 divide-y divide-gray-300',
     legend: 'flex flex-row-reverse w-full justify-between mb-2',
     addButton: 'mdi mdi-plus-circle-outline focus:outline-none focus:ring-2 focus:ring-sinai-red focus:border-sinai-red cursor-pointer mx-1',
-    label: 'block text-sm font-medium text-gray-700',
+    label: 'block text-base font-medium text-gray-700',
     itemWrapper: '',
     noData: 'px-3 py-2 text-sm bg-gray-200',
     itemToolbar: '!m-0 p-2 border-b bg-gray-200',
@@ -41,7 +41,7 @@ const customStyles = {
   },
   // custom classes
   container: {
-    label: 'flex items-center justify-between',
+    label: 'flex items-center',
     checkbox: 'relative flex items-center gap-x-1 my-1',
     checkboxList: 'flex flex-wrap gap-x-4',
     arrayList: 'flex items-center gap-x-2',
@@ -50,7 +50,7 @@ const customStyles = {
     wrapper: 'flex items-center gap-x-2',
   },
   tooltip: {
-    icon: 'mdi mdi-information-slab-circle-outline cursor-pointer',
+    icon: 'text-base mdi mdi-information-slab-circle-outline cursor-pointer',
   },
 }
 
