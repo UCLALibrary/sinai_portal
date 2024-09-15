c3cf6a2ce60b2187a40e9636a98a1a23a4ab3caa
Douglas Kim <dougkim@15solutions.com>
2024-09-15 15:13:45 -0700

    Configure Algolia and index agents using Algolia Scout driver.

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Traits\JsonSchemas;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Agent extends Model
 {
-    use HasFactory, JsonSchemas;
+    use HasFactory, JsonSchemas, Searchable;
 
     protected $keyType = 'string';
     public $incrementing = false;
@@ -31,6 +32,58 @@ class Agent extends Model
         'pref_name',
         'json',
     ];
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        // decode json field
+        $data = json_decode($this->json, true);
+
+        // ark
+        $array['ark'] = $data['ark'] ?? null;
+
+        // gender
+        $array['gender'] = $data['gender'] ?? null;
+
+        // birth
+        $birth = $data['birth'] ?? null;
+        if ($birth) {
+            $array['birth_value'] = $birth['value'] ? 'b. ' . $birth['value'] : null;
+            $array['birth_not_before'] = $birth['iso']['not_before'] ?? null;
+            $array['birth_not_after'] = $birth['iso']['not_after'] ?? null;
+        }
+
+        // death
+        $death = $data['death'] ?? null;
+        if ($death) {
+            $array['death_value'] = $death['value'] ? 'd. ' . $death['value'] : null;
+            $array['death_not_before'] = $death['iso']['not_before'] ?? null;
+            $array['death_not_after'] = $death['iso']['not_after'] ?? null;
+        }
+
+        // floruit
+        $floruit = $data['floruit'] ?? null;
+        if ($floruit) {
+            $array['floruit_value'] = $floruit['value'] ? 'fl. ' . $floruit['value'] : null;
+            $array['floruit_not_before'] = $floruit['iso']['not_before'] ?? null;
+            $array['floruit_not_after'] = $floruit['iso']['not_after'] ?? null;
+        }
+
+        /*
+         * Apply default transformations if desired.
+         * 
+         * https://www.algolia.com/doc/framework-integration/laravel/indexing/configure-searchable-data/?client=php#transformers
+         */
+        // $array = $this->transform($array);
+
+        return $array;
+    }
 }
 
 /*
