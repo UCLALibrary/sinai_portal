75f3b719f195e8872d618fa96f963cb1e8a3c73b
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2024-09-18 16:53:54 +0200

    Improve responsive styles for agents list view

--- resources/js/Shared/Search/AgentResult.vue
+++ resources/js/Shared/Search/AgentResult.vue
@@ -1,25 +1,25 @@
 <template>
   <a :href="'/agents/' + result.id">
-    <div class="flex">
-      <div class="w-[85%] sm:w-[55%] cursor-pointer">
+    <div class="flex flex-col lg:flex-row">
+      <div class="w-full w-[85%] sm:w-[55%] cursor-pointer">
         <h3 class="font-medium max-w-5xl pr-4">
           {{ result.pref_name }}
         </h3>
       </div>
   
-      <div class="sm:w-[25%] flex cursor-pointer">
+      <div class="w-full sm:w-[25%] cursor-pointer">
         <p class="pr-3 sm:pr-4">
           {{ result.type }}
         </p>
       </div>
   
-      <div class="sm:w-[15%] flex cursor-pointer">
+      <div class="w-full sm:w-[15%] cursor-pointer">
         <p class="pr-3 sm:pr-4">
           {{ result.gender }}
         </p>
       </div>
 
-       <div class="sm:w-[45%] flex cursor-pointer">
+       <div class="w-full sm:w-[45%] cursor-pointer">
         <p class="flex pr-3 sm:pr-4">
           <div>{{ result.birth_value }}</div>
           <div>{{ result.death_value }}</div>
@@ -27,7 +27,7 @@
         </p>
       </div>
   
-      <div class="sm:w-[45%] flex cursor-pointer">
+      <div class="w-full sm:w-[45%] cursor-pointer">
         <p class="pr-3 sm:pr-4 text-sm">
           {{ result.ark }}
         </p>
