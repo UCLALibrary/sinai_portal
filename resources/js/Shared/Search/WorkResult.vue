a523b7eb25202d6320ba226742c11fd23a8b5e7b
Tomas Germann <t@herrmanngermann.ch>
2024-09-23 13:14:24 +0200

    Update styles for agents and works

--- resources/js/Shared/Search/WorkResult.vue
+++ resources/js/Shared/Search/WorkResult.vue
@@ -2,24 +2,24 @@
   <a :href="'/works/' + result.id">
     <div class="flex flex-col lg:flex-row">
       <div class="w-full lg:w-4/12">
-        <h3 class="font-medium text-lg pr-4">
+        <h3 class="font-medium text-lg pr-4 tracking-wide">
           {{ result.pref_title }}
         </h3>
       </div>
   
-      <div class="w-full lg:w-3/12">
+      <div class="w-full lg:w-1/12">
         <p class="pr-3">
           {{ result.orig_lang_label }}
         </p>
       </div>
   
-      <div class="w-full lg:w-4/12">
+      <div class="w-full lg:w-3/12">
         <p class="pr-3">
           {{ result.genre.join('; ') }}
         </p>
       </div>
 
-      <div class="w-full lg:w-3/12">
+      <div class="w-full lg:flex-1">
         <p class="pr-3">
           {{ result.creation_value }}
         </p>
