d8eeac3c5d37ef348881362ea23df7bf602c6cd5
Lukas Märki <maerki@luukas.ch>
2024-10-05 18:17:45 +0200

    add seeder for roles and permissions

---
+++ database/seeders/RolesPermissionsSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+		$frontendUser = Role::findOrCreate('frontend_user');
+		$editor = Role::findOrCreate('editor');
+		$admin = Role::findOrCreate('admin');
+
+		$createUser = Permission::findOrCreate('create user');
+		$readUser = Permission::findOrCreate('read user');
+		$updateUser = Permission::findOrCreate('update user');
+		$deleteUser = Permission::findOrCreate('delete user');
+
+		$createRecord = Permission::findOrCreate('create record');
+		$readRecord = Permission::findOrCreate('read record');
+		$updateRecord = Permission::findOrCreate('update record');
+		$deleteRecord = Permission::findOrCreate('delete record');
+
+		$editor->givePermissionTo($createRecord)
+			->givePermissionTo($readRecord)
+			->givePermissionTo($updateRecord)
+			->givePermissionTo($deleteRecord);
+
+		$admin->givePermissionTo($createRecord)
+			->givePermissionTo($readRecord)
+			->givePermissionTo($updateRecord)
+			->givePermissionTo($deleteRecord)
+			->givePermissionTo($createUser)
+			->givePermissionTo($readUser)
+			->givePermissionTo($updateUser)
+			->givePermissionTo($deleteUser);
+
+
+		$rafaelSchwemmer = User::where('email', 'rafael.schwemmer@textandbytes.com')->first();
+		if($rafaelSchwemmer) {
+			$rafaelSchwemmer->assignRole($admin);
+		}
+
+		$douglasKim = User::where('email', 'dougkim@15solutions.com')->first();
+		if($douglasKim) {
+			$douglasKim->assignRole($admin);
+		}
+
+		$lukasMaerki = User::where('email', 'lukas@inventic.ch')->first();
+		if($lukasMaerki) {
+			$lukasMaerki->assignRole($admin);
+		}
+
+		$dawnChildress = User::where('email', 'dchildress@library.ucla.edu')->first();
+		if($dawnChildress) {
+			$dawnChildress->assignRole($admin);
+		}
+
+		$williamPotter = User::where('email', 'williampotter@library.ucla.edu')->first();
+		if($williamPotter) {
+			$williamPotter->assignRole($admin);
+		}
+    }
+}
