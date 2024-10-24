7e306d51cb0bde774fac147017ad5e05bf1498d5
Douglas Kim <dougkim@15solutions.com>
2024-10-23 19:46:21 -1000

    Display error message when uploading incompatible JSON files that do not align with the JSON schema for the given resource type.

--- app/Http/Controllers/Api/FilesController.php
+++ app/Http/Controllers/Api/FilesController.php
@@ -15,27 +15,35 @@ class FilesController extends Controller
      */
     public function storeOnUpload(JsonFileUploadRequest $request, $resourceName): JsonResponse
     {
-        $file = $request->file('files');
+        try {
+            $file = $request->file('files');
 
-        // decode the json file
-        $json = $file->get();
-        $data = json_decode($json, true);
+            // decode the json file
+            $json = $file->get();
+            $data = json_decode($json, true);
 
-        // get the model class using the singular version of the resource name
-        $modelClass = '\\App\\Models\\' . ucfirst(Str::singular($resourceName));
+            // get the model class using the singular version of the resource name
+            $modelClass = '\\App\\Models\\' . ucfirst(Str::singular($resourceName));
 
-        // populate the fillable fields for the resource
-        $instance = new $modelClass();
-        $fields = $instance->getFillableFields($data, $json);
+            // populate the fillable fields for the resource
+            $instance = new $modelClass();
+            $fields = $instance->getFillableFields($data, $json);
 
-        // create the resource
-        $resource = $modelClass::create($fields);
+            // create the resource
+            $resource = $modelClass::create($fields);
 
-        return response()->json([
-            'status' => $resource ? 'success' : 'error',
-            'message' => $resource ? 'The JSON file has been successfully uploaded.' : 'Error uploading the JSON file.',
-            'resourceId' => basename($data['ark']),
-        ]);
+            return response()->json([
+                'status' => $resource ? 'success' : 'error',
+                'message' => $resource ? 'The JSON file has been successfully uploaded.' : 'Error uploading the JSON file.',
+                'resourceId' => basename($data['ark']),
+            ]);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'The uploaded JSON files are incompatible with the ' . Str::singular($resourceName) . ' JSON schema.<br><br>' . $e->getMessage(),
+            ]);
+        }
     }
 
     /**
@@ -43,70 +51,86 @@ class FilesController extends Controller
      */
     public function updateOnUpload(JsonFileUploadRequest $request, $resourceName, $resourceId): JsonResponse
     {
-        $file = $request->file('files');
+        try {
+            $file = $request->file('files');
 
-        // decode the json file
-        $json = $file->get();
-        $data = json_decode($json, true);
-
-        // get the model class using the singular version of the resource name
-        $modelClass = '\\App\\Models\\' . ucfirst(Str::singular($resourceName));
-
-        // get the resource with the given id
-        $resource = $modelClass::find($resourceId);
-
-        // populate the fillable fields for the resource
-        $fields = $resource->getFillableFields($data, $json);
-
-        // update the resource
-        $status = $resource->update($fields);
-
-        return response()->json([
-            'status' => $status ? 'success' : 'error',
-            'message' => $status ? 'The JSON file has been successfully uploaded.' : 'Error uploading the JSON file.',
-        ]);
-    }
-
-    public function batchUpload(JsonBatchUploadRequest $request, $resourceName): JsonResponse
-    {
-        $messages = [];
-
-        $files = $request->file('files');
-        foreach ($files as $file) {
             // decode the json file
             $json = $file->get();
             $data = json_decode($json, true);
 
             // get the model class using the singular version of the resource name
-            $resourceType = ucfirst(Str::singular($resourceName));
-            $modelClass = '\\App\\Models\\' . $resourceType;
-
-            // populate the fillable fields for the resource
-            $instance = new $modelClass();
-            $fields = $instance->getFillableFields($data, $json);
+            $modelClass = '\\App\\Models\\' . ucfirst(Str::singular($resourceName));
 
             // get the resource with the given id
-            $resourceId = basename($fields['id']);
             $resource = $modelClass::find($resourceId);
 
-            if ($resource) {
-                // update the resource
-                if ($resource->update($fields)) {
-                    $messages[] = $resourceType . " with '" . $data['ark'] . "' has been updated.";
-                }
-            }
-            else {
-                // create the resource
-                if ($modelClass::create($fields)) {
-                    $messages[] = $resourceType . " with '" . $data['ark'] . "' has been created.";
-                    $createdResources[$resourceId] = $data['ark'];
-                }
-            }
-        }
+            // populate the fillable fields for the resource
+            $fields = $resource->getFillableFields($data, $json);
 
-        return response()->json([
-            'status' => count($messages) > 0 ? 'success' : 'error',
-            'message' => count($messages) > 0 ? implode('<br>', $messages) : 'Error uploading JSON file(s).',
-        ]);
+            // update the resource
+            $status = $resource->update($fields);
+
+            return response()->json([
+                'status' => $status ? 'success' : 'error',
+                'message' => $status ? 'The JSON file has been successfully uploaded.' : 'Error uploading the JSON file.',
+            ]);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'The uploaded JSON files are incompatible with the ' . Str::singular($resourceName) . ' JSON schema.<br><br>' . $e->getMessage(),
+            ]);
+        }
+    }
+
+    public function batchUpload(JsonBatchUploadRequest $request, $resourceName): JsonResponse
+    {
+        try {
+            $messages = [];
+
+            $files = $request->file('files');
+            foreach ($files as $file) {
+                // decode the json file
+                $json = $file->get();
+                $data = json_decode($json, true);
+
+                // get the model class using the singular version of the resource name
+                $resourceType = ucfirst(Str::singular($resourceName));
+                $modelClass = '\\App\\Models\\' . $resourceType;
+
+                // populate the fillable fields for the resource
+                $instance = new $modelClass();
+                $fields = $instance->getFillableFields($data, $json);
+
+                // get the resource with the given id
+                $resourceId = basename($fields['id']);
+                $resource = $modelClass::find($resourceId);
+
+                if ($resource) {
+                    // update the resource
+                    if ($resource->update($fields)) {
+                        $messages[] = $resourceType . " with '" . $data['ark'] . "' has been updated.";
+                    }
+                }
+                else {
+                    // create the resource
+                    if ($modelClass::create($fields)) {
+                        $messages[] = $resourceType . " with '" . $data['ark'] . "' has been created.";
+                        $createdResources[$resourceId] = $data['ark'];
+                    }
+                }
+            }
+
+            return response()->json([
+                'status' => count($messages) > 0 ? 'success' : 'error',
+                'message' => count($messages) > 0 ? implode('<br>', $messages) : 'Error uploading JSON file(s).',
+            ]);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'The uploaded JSON files are incompatible with the ' . Str::singular($resourceName) . ' JSON schema.<br><br>' . $e->getMessage(),
+            ]);
+        }
     }
 }
