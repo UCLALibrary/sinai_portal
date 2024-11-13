24081e489cc9f0c90669e6197d2496605c765f87
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2024-11-13 21:39:15 +0100

    Implement database migration that creates a generated column "jsonb" of type "jsonb" from the "json" column that is of type "json" so we can keep the original structure of the JSON in the "json" column while benefitting from the querying capabilities and performance gains of the the "jsonb" column

---
+++ database/migrations/2024_11_13_212400_add_generated_jsonb_columns.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $tables = [
+            'agents',
+            'bibliographies',
+            'contents',
+            'layers',
+            'manuscripts',
+            'parts',
+            'places',
+            'works'
+        ];
+
+        foreach ($tables as $table) {
+            DB::statement("ALTER TABLE $table ADD COLUMN jsonb jsonb GENERATED ALWAYS AS (json::jsonb) STORED");
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $tables = [
+            'agents',
+            'bibliographies',
+            'contents',
+            'layers',
+            'manuscripts',
+            'parts',
+            'places',
+            'works'
+        ];
+
+        foreach ($tables as $table) {
+            DB::statement("ALTER TABLE $table DROP COLUMN jsonb");
+        }
+    }
+};
