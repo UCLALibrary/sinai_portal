1b4d5359d3ac7e089bd738fd541a78dcfd6a8643
Douglas Kim <dougkim@15solutions.com>
2024-11-20 15:27:51 -0800

    Index min and max dates values from the "assoc_date > iso > not_before" and "assoc_date > iso > not_after" fields from parent layers for text units.

--- app/Models/TextUnit.php
+++ app/Models/TextUnit.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasRelatedEntities;
 use App\Traits\JsonSchemas;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,7 +10,7 @@ use Laravel\Scout\Searchable;
 
 class TextUnit extends Model
 {
-    use HasFactory, JsonSchemas, Searchable;
+    use HasFactory, JsonSchemas, Searchable, HasRelatedEntities;
     
     protected $keyType = 'string';
     public $incrementing = false;
@@ -40,21 +41,59 @@ class TextUnit extends Model
             ],
         ],
     ];
-    
+
+    /**
+     * Get the parent layers.
+     *
+     * @return string|null
+     */
+    public function getParentLayers()
+    {
+        $data = $this->getJsonData();
+
+        return !empty($data['parent'])
+            ? Layer::whereIn('ark', $data['parent'])->get()
+            : null;
+    }
+
     public function toSearchableArray(): array
     {
         $array = $this->toArray();
-        
+
         $array['ark'] = $this->ark ?? null;
         $array['label'] = $this->label ?? null;
+
+        // min and max date from the 'assoc_date' field from layers of type 'origin'
+        foreach($this->getParentLayers() as $layer) {
+            $data = $layer->getJsonData();
+
+            if (isset($data['assoc_date'])) {
+                $notBeforeValues = [];
+                $notAfterValues = [];
+                foreach($data['assoc_date'] as $date) {
+                    if (isset($date['type']['id']) && $date['type']['id'] === 'origin') {
+                        $notBeforeValues[] = $date['iso']['not_before'] ?? null;
+                        $notAfterValues[] = $date['iso']['not_after'] ?? null;
+                    }
+                }
+    
+                // minimum date from the 'not_before' field from layers of type 'origin'
+                $values = array_filter($notBeforeValues, fn($value) => $value !== null);
+                $array['date_min'] = $values ? min(array_map('intval', $values)) : null;
         
+                // maximum date from the 'not_after' field from layers of type 'origin'
+                $values = array_filter($notAfterValues, fn($value) => $value !== null);
+                $array['date_max'] = $values ? max(array_map('intval', $values)) : null;
+            }
+        }
+
         /*
          * Apply default transformations if desired.
          *
          * https://www.algolia.com/doc/framework-integration/laravel/indexing/configure-searchable-data/?client=php#transformers
          */
         // $array = $this->transform($array);
-        
+
         return $array;
     }
 }
