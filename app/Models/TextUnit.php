495b371809cab40f77722bfe1c4f709b1cf77a40
Douglas Kim <dougkim@15solutions.com>
2024-11-20 15:07:53 -0800

    Fix spacing.

--- app/Models/TextUnit.php
+++ app/Models/TextUnit.php
@@ -10,52 +10,56 @@ use Laravel\Scout\Searchable;
 class TextUnit extends Model
 {
     use HasFactory, JsonSchemas, Searchable;
-	
-	protected $keyType = 'string';
-	public $incrementing = false;
-	
-	protected $fillable = [
-		'id',
-		'ark',
-		'label',
-		'json',
-	];
-	
-	public function getFillableFields($data, $json)
-	{
-		return array_combine($this->fillable, [
-			basename($data['ark']),  // use the trailing ark segment as the id
-			$data['ark'],
-			$data['label'],
-			$json,
-		]);
-	}
-	
-	public static $config = [
-		'index' => [
-			'columns' => [
-				'id' => 'Id',
-				'ark' => 'Ark',
-				'label' => 'Label',
-			],
-		],
-	];
-	
-	public function toSearchableArray(): array
-	{
-		$array = $this->toArray();
-		
-		$array['ark'] = $this->ark ?? null;
-		$array['label'] = $this->label ?? null;
-		
-		/*
-		 * Apply default transformations if desired.
-		 *
-		 * https://www.algolia.com/doc/framework-integration/laravel/indexing/configure-searchable-data/?client=php#transformers
-		 */
-		// $array = $this->transform($array);
-		
-		return $array;
-	}
+    
+    protected $keyType = 'string';
+    public $incrementing = false;
+    
+    protected $fillable = [
+        'id',
+        'ark',
+        'label',
+        'json',
+    ];
+    
+    public function getFillableFields($data, $json)
+    {
+        return array_combine($this->fillable, [
+            basename($data['ark']),  // use the trailing ark segment as the id
+            $data['ark'],
+            $data['label'],
+            $json,
+        ]);
+    }
+    
+    public static $config = [
+        'index' => [
+            'columns' => [
+                'id' => 'Id',
+                'ark' => 'Ark',
+                'label' => 'Label',
+            ],
+        ],
+    ];
+    
+    public function toSearchableArray(): array
+    {
+        $array = $this->toArray();
+        
+        $array['ark'] = $this->ark ?? null;
+        $array['label'] = $this->label ?? null;
+        
+        /*
+         * Apply default transformations if desired.
+         *
+         * https://www.algolia.com/doc/framework-integration/laravel/indexing/configure-searchable-data/?client=php#transformers
+         */
+        // $array = $this->transform($array);
+        
+        return $array;
+    }
 }
+
+/*
+ * Execute the static initializer to load the schemas for JSON Forms.
+ */
 TextUnit::initialize('text_unit');
