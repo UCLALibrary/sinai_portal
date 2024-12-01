8a109b3c90833546d4c2ab8c1eb4fc75f5243f38
Douglas Kim <dougkim@15solutions.com>
2024-12-01 12:35:25 -0800

    Adjust spacing.

--- database/seeders/RolesPermissionsSeeder.php
+++ database/seeders/RolesPermissionsSeeder.php
@@ -15,64 +15,63 @@ class RolesPermissionsSeeder extends Seeder
      */
     public function run(): void
     {
-		$frontendUser = Role::findOrCreate('Frontend user');
-		$editor = Role::findOrCreate('Editor');
-		$admin = Role::findOrCreate('Admin');
+        $frontendUser = Role::findOrCreate('Frontend user');
+        $editor = Role::findOrCreate('Editor');
+        $admin = Role::findOrCreate('Admin');
 
-		$createUser = Permission::findOrCreate('create user');
-		$readUser = Permission::findOrCreate('read user');
-		$updateUser = Permission::findOrCreate('update user');
-		$deleteUser = Permission::findOrCreate('delete user');
+        $createUser = Permission::findOrCreate('create user');
+        $readUser = Permission::findOrCreate('read user');
+        $updateUser = Permission::findOrCreate('update user');
+        $deleteUser = Permission::findOrCreate('delete user');
 
-		$createRecord = Permission::findOrCreate('create record');
-		$readRecord = Permission::findOrCreate('read record');
-		$updateRecord = Permission::findOrCreate('update record');
-		$deleteRecord = Permission::findOrCreate('delete record');
-		$uploadRecord = Permission::findOrCreate('upload record');
-	  
-		$viewCms = Permission::findOrCreate('view cms');  
+        $createRecord = Permission::findOrCreate('create record');
+        $readRecord = Permission::findOrCreate('read record');
+        $updateRecord = Permission::findOrCreate('update record');
+        $deleteRecord = Permission::findOrCreate('delete record');
+        $uploadRecord = Permission::findOrCreate('upload record');
+      
+        $viewCms = Permission::findOrCreate('view cms');  
 
-		$editor->givePermissionTo($createRecord)
-			->givePermissionTo($readRecord)
-			->givePermissionTo($updateRecord)
-			->givePermissionTo($deleteRecord)
-			->givePermissionTo($viewCms);
+        $editor->givePermissionTo($createRecord)
+            ->givePermissionTo($readRecord)
+            ->givePermissionTo($updateRecord)
+            ->givePermissionTo($deleteRecord)
+            ->givePermissionTo($viewCms);
 
-		$admin->givePermissionTo($createRecord)
-			->givePermissionTo($readRecord)
-			->givePermissionTo($updateRecord)
-			->givePermissionTo($deleteRecord)
-			->givePermissionTo($uploadRecord)
-			->givePermissionTo($createUser)
-			->givePermissionTo($readUser)
-			->givePermissionTo($updateUser)
-			->givePermissionTo($deleteUser)
-			->givePermissionTo($viewCms);
+        $admin->givePermissionTo($createRecord)
+            ->givePermissionTo($readRecord)
+            ->givePermissionTo($updateRecord)
+            ->givePermissionTo($deleteRecord)
+            ->givePermissionTo($uploadRecord)
+            ->givePermissionTo($createUser)
+            ->givePermissionTo($readUser)
+            ->givePermissionTo($updateUser)
+            ->givePermissionTo($deleteUser)
+            ->givePermissionTo($viewCms);
 
+        $rafaelSchwemmer = User::where('email', 'rafael.schwemmer@textandbytes.com')->first();
+        if ($rafaelSchwemmer) {
+            $rafaelSchwemmer->assignRole($admin);
+        }
 
-		$rafaelSchwemmer = User::where('email', 'rafael.schwemmer@textandbytes.com')->first();
-		if($rafaelSchwemmer) {
-			$rafaelSchwemmer->assignRole($admin);
-		}
+        $douglasKim = User::where('email', 'dougkim@15solutions.com')->first();
+        if ($douglasKim) {
+            $douglasKim->assignRole($admin);
+        }
 
-		$douglasKim = User::where('email', 'dougkim@15solutions.com')->first();
-		if($douglasKim) {
-			$douglasKim->assignRole($admin);
-		}
+        $lukasMaerki = User::where('email', 'lukas@inventic.ch')->first();
+        if ($lukasMaerki) {
+            $lukasMaerki->assignRole($admin);
+        }
 
-		$lukasMaerki = User::where('email', 'lukas@inventic.ch')->first();
-		if($lukasMaerki) {
-			$lukasMaerki->assignRole($admin);
-		}
+        $dawnChildress = User::where('email', 'dchildress@library.ucla.edu')->first();
+        if ($dawnChildress) {
+            $dawnChildress->assignRole($admin);
+        }
 
-		$dawnChildress = User::where('email', 'dchildress@library.ucla.edu')->first();
-		if($dawnChildress) {
-			$dawnChildress->assignRole($admin);
-		}
-
-		$williamPotter = User::where('email', 'williampotter@library.ucla.edu')->first();
-		if($williamPotter) {
-			$williamPotter->assignRole($admin);
-		}
+        $williamPotter = User::where('email', 'williampotter@library.ucla.edu')->first();
+        if ($williamPotter) {
+            $williamPotter->assignRole($admin);
+        }
     }
 }
