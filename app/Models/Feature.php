ddc8e6d31084a0f023e6dda5841d68bb1ccf6d06
Douglas Kim <dougkim@15solutions.com>
2024-12-09 18:54:54 -0800

    Allow id field for features to be editable. Note: When no id field is supplied on the create and edit forms, the id field is auto-generated as a slug from the label field.

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -21,6 +21,7 @@ class Feature extends Model
      * @var array
      */
     protected $fillable = [
+        'id',
         'label',
         'corresp_note',
         'summary',
@@ -33,6 +34,7 @@ class Feature extends Model
     public function getFillableFields($data)
     {
         return array_combine($this->fillable, [
+            $data['id'] ?? Str::slug($data['label'], '-'),
             $data['label'],
             $data['corresp_note'] ?? null,
             $data['summary'] ?? null,
@@ -53,14 +55,6 @@ class Feature extends Model
             ],
         ],
     ];
-
-    public static function booted() {
-        static::creating(function ($model) {
-            if(!isset($model->id)) {
-                $model->id = Str::slug($model->label, '-');
-            }
-        });
-    }
 
     public function formContexts(): BelongsToMany
     {
