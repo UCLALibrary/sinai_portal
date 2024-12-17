63ead6a11c70091da17d297e1475d5ce407e9629
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2024-12-17 17:41:21 +0100

    Update home page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -6,21 +6,36 @@
     <FrontendLayout title="Sinai Manuscripts Data Portal">
         
         <div class="flex justify-center mt-16 mb-16">
-            <img src="/sinai-logo.svg" alt="Logo" class="w-32 h-32" />
+            <p class="text-center">
+                The Sinai Manuscripts Data Portal provides access to the structured descriptions of manuscripts from St. Catherine’s Monastery in Sinai, Egypt. The Portal presents manuscripts as physical containers that comprise assemblages of inscribed layers and texts. This modular approach results in discrete records for each Text Unit, Inscribed Layer, and Manuscript Object, while documenting the connections between these descriptive components. The Portal also serves as an authority file for the works, people, organizations, and places connected to the manuscripts. As an evolving project, many of the records contain only basic descriptions; however, description is ongoing, with updates published regularly. For more information about the Portal, description conventions, and the underlying data model, see the About pages.
+            </p>
         </div>
         <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 m-16">
-            <div class="bg-white dark:bg-black dark:text-white rounded-lg shadow-lg p-6">
-                <h2 class="font-dosis text-2xl">About</h2>
-                <p class="mt-4 text-lg">
-                    As the new centralized data store for the Sinai Manuscripts Digital Library website, the Sinai Manuscripts Data Portal will house records for all Sinai-related data. The database will store all cataloguing and descriptive data collected, as well as any future descriptive work. Project scholars and cataloguers will be able to submit new descriptive metadata through the application’s administrative interface. 
-                </p>
+            <div class="bg-white dark:bg-black dark:text-white rounded-lg shadow-lg p-6 flex flex-col">
+                <h2 class="font-dosis text-2xl">Works</h2>
+                <div class="flex flex-col h-full justify-between">
+                    <p class="mt-4 text-lg">
+                        Browse authority records for works attested in the Sinai manuscripts.
+                    </p>
+                   <p class="">
+                        <a :href="route('frontend.works.index')" class="mt-auto text-sinai-dark-blue">
+                            Works
+                        </a>
+                   </p>
+               </div>
             </div>
-            <div class="bg-white dark:bg-black dark:text-white rounded-lg shadow-lg p-6">
-                <h2 class="font-dosis
-                text-2xl">Browse</h2>
-                <p class="mt-4 text-lg">
-                    The portal provides access to detailed metadata of the Sinai Manuscripts Digital Library. Users can search, browse, and download the the metadata, and contribute to the project.
-                </p>
+            <div class="bg-white dark:bg-black dark:text-white rounded-lg shadow-lg p-6 flex flex-col">
+                <h2 class="font-dosis text-2xl">Agents</h2>
+                <div class="flex flex-col h-full justify-between">
+                    <p class="mt-4 text-lg">
+                        Browse authority records for people and organizations mentioned in or related to the Sinai manuscripts, including authors of attested works, scribes, owners, patrons, and more.
+                    </p>
+                    <p class="">
+                        <a :href="route('frontend.agents.index')" class="mt-auto text-sinai-dark-blue">
+                            Agents
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>
     </FrontendLayout>
