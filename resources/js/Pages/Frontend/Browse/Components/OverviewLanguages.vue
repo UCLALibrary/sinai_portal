bee6185404191f32254cddde2e81396fc716e05a
Lukas Märki <maerki@luukas.ch>
2024-12-20 15:09:24 +0100

    Add plural 's' to 'Languages' label when multiple languages are present

--- resources/js/Pages/Frontend/Browse/Components/OverviewLanguages.vue
+++ resources/js/Pages/Frontend/Browse/Components/OverviewLanguages.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="languages && languages.length > 0" class="item-container">
-    <span class="item-label">Primary languages</span>
+    <span class="item-label">Primary language<template v-if="languages.length > 1">s</template></span>
     <p class="item-value">
       {{ languages.map(lang => lang.label).join(', ') }}
     </p>
